320ed5c9ec4005e5a8519b2cbc0e1cf469805ca6
Yadab Raj Ojha <luckyyadab@gmail.com>
2015-11-25 11:10:01 +0545

    JDBC conn test saved.

--- src/jdbc/JdbcConnectionTest.java
+++ src/jdbc/JdbcConnectionTest.java
@@ -3,30 +3,45 @@ package jdbc;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 
 public class JdbcConnectionTest {
-	public static void main( String[] args ) {
-		
+	public static void main(String[] args) {
+
+		Connection con = null;
 		try {
-			Class.forName( "com.mysql.jdbc.Driver" );
+			// 1. Load Driver
+			Class.forName("com.mysql.jdbc.Driver");
 
-			Connection con = DriverManager.getConnection( "jdbc:mysql://localhost:3306/testdb", "root", "" );
+			// 2.Connect to the Database: URL:database Path, username, password.
 			// here studentdb is database name, root is username and password
+			con = DriverManager.getConnection(
+					"jdbc:mysql://localhost:3306/studentdb", "root", "");
 
-			Statement stmt = con.createStatement( );
+			// 3. Create Statement
+			Statement stmt = con.createStatement();
 
-			ResultSet rs = stmt.executeQuery( "select * from student" );
+			// 4. Execute Query
+			ResultSet rs = stmt.executeQuery("select * from student");
 
-			while ( rs.next( ) ) {// Loop Each Row
+			// 5. Fetch the result.
+			while (rs.next()) {// Loop Each Row
 				// Fetch columns
-				System.out.println( rs.getInt( 1 ) + "  " + rs.getString( 2 ) );
+				// System.out.println(rs.getInt(1) + "  " + rs.getString(2));
+				System.out.println(rs.getInt("id") + "  " + rs.getString("name"));
 			}
 
-			con.close( );
+		} catch (Exception e) {
+			e.printStackTrace();
 
-		} catch ( Exception e ) {
-			System.out.println( e );
+		} finally {
+			// 6. Close connection.
+			try {
+				con.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
 		}
 
 	}
